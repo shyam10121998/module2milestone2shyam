e65425d690bdbbcbcfe53a1b8ed782760d68d064
shyam10121998 <shyamrachchh77@gmail.com>
2023-10-17 15:20:09 -0800

    Add Main Structure To The index.view.php File

---
+++ index.view.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<img class="banner" src="/images/banner.png" width="100%">
+
+<!--product categories-->
+
+<div class="content-wrap content">
+<div class="cards">
+<div class="card">
+<img src="/images/product category.png" alt="Image 1">
+<div class="card-text">
+<h2>Japenese Maples</h2>
+</div>
+</div>
+
+<div class="card">
+<img src="/images/product category.png" alt="Image 2">
+<div class="card-text">
+<h2>Deciduous</h2>
+</div>
+</div>
+
+<div class="card">
+<img src="/images/product category.png" alt="Image 3">
+<div class="card-text">
+<h2>Evergreen</h2>
+</div>
+</div>  
+</div>
+<div class="btn-container">
+<a href="products.html" class="btn">See All</a>
+</div>
+</div>
+
+<!--what we offer-->
+
+<div class="content whatweoffer">
+<h1>What we offer</h1>
+<p>
+We offer products such conifers,evergreen, vines, grasses, edibles and bamboos.Apart from plants and trees, we offer GIFT card certificates and Delivery serives as well.
+</p>
+<a href="services.html">Click here to know more</a>
+</div>
+
+<!--Help-->
+
+<div class="help content" >
+<div class="help-text">
+<h1>We Are Here to Help You</h1>
+<p>If you have any questions or need assistance, feel free to reach out to us. Our team is ready to assist you.</p>
+</div>
+<div class="search-bar">
+<input type="text" placeholder="Ask a question here">
+<a href="">submit</a>
+</div>
+</div>
+
+<!--our story-->
+
+<div class="ourstory content">
+<h1>Our Story</h1>
+<h2>Welcome to Outback Nursery & Garden Centre
+6016 Headquarters Road, Courtenay BC V9J 1M7</h2>
+<p>  Outback Nursery is one of the largest garden centres on Vancouver Island.   Our nursery is situated on a  6 acre property and offers 5 overflowing greenhouses (30x170ft); almost 23,000 square feet.
+Outback Nursery began in 1998-1999 when our parents retired to the island from Aldergrove. They asked if we'd be interested in joining them and then purchased this  heavily treed property. We began clearing some trees and settled into country life trying our hand at raising some turkeys and pigs as well as growing some hedging and conifers. We joined the Farmer's Market, then as our inventory expanded we began running a spec truck down island stopping here and there to show our quality product to potential customers.
+
+Little by little the property was cleared and one by one the greenhouses and irrigation zones increased to what you see today. In the beginning we were limited to wholesale and landscape clientele, then somewhere along the way interest in our nursery grew and we opened to the public.
+
+</p>
+</div>
+
+<!--reviews-->
+
+<h1 class="reviewh1 content">Recent reviews</h1>
+<div class="container content">
+<div class="block">
+<div class="circle-image">
+<img src="/images/product category.png" alt="Image 1">
+</div>
+<div class="text">
+<p>
+We have been coming to Outback for eight years now, and it's our favourite nursery on Vancouver Island. Read on to see why... Outback is a three-generation family owned and operated nursery. Everyone is involved and passionate about growing healthy happy plants. We love walking through the 5 greenhouses full of Japanese maples and other foundation trees and shrubs. It's like taking a Japanese forest bath.
+
+Outback's selection of Japanese maples is the best we've ever seen. They propagate their own trees, and grow them in abundance. If we describe the qualities we want, and the location, they will supply us with a tree that will thrive, and which will become a feature of our garden.
+
+Annette is wonderfully knowledgeable about plants in general, and her plants in particular. She and her family work hard to choose plants that will do well in our climate, and look good at the same time.
+
+Their plants are healthy. We've never been sent home with plants that are sickly or misshapen. The more we have learned about how they transplant, propagate, and grow their plants, the more trust we have in their stock.
+
+The advice we get from them is accurate and comprehensive. Whether it's planting instruction, or ideas for companion plants, or suggestions for plant care, their advice works.
+
+The entire family (staff) is warm and personable. We feel that we are more than customers, and the care we receive makes us want to come back.
+
+Did I mention fair prices? Outback sells plants at excellent prices, and that makes it much more affordable to create a beautiful garden.
+
+With great appreciation, <strong>Don and Michael</strong>
+
+
+</p>
+</div>
+</div>
+<div class="block">
+<div class="circle-image">
+<img src="/images/product category.png" alt="Image 2">
+</div>
+<div class="text">
+<p>
+We recently moved from Vancouver to the Ships Point area to 1.5 acres of garden. Our experience with the family run Outback Nursery over the past several years has been exceptional. Their knowledge of plants combined with their very friendly and helpful service has made our experience enjoyable. Equally important is their excellent pricing, the unique and wide range of plants, shrubs, trees. They also have beautiful rich mulches and soils for the garden. We are very lucky to have found Outback Nursery, the family has helped us make our garden a success.
+<strong>Liane and JP Cardinal</strong>
+</p>
+</div>
+</div>
+</div>
+
+</body>
+</html>
