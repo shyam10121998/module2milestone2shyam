b941988481f5c29833c6f5736fbb24df3d698da7
shyam10121998 <shyamrachchh77@gmail.com>
2023-10-17 15:46:50 -0800

    Add HTML Structure For Footer

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+        <div class="footer-content">
+            <h1>Outback Nursery</h1>
+            <p>Nature's Palette, Our Expertise</p>
+            <div class="social-icons">
+                <a href="tel:2507030723" target="_blank"><img src="/images/phone.png" alt="Phone"></a>
+                <a href="https://www.facebook.com/p/Outback-Nursery-Garden-Centre-100045470087619/" target="_blank"><img src="/images/facebook-icon-512x512-seb542ju.png" alt="Facebook"></a>
+                <a href="https://www.instagram.com/outbacknursery/?hl=en" target="_blank"><img src="images/Instagram-Icon.png" alt="Instagram"></a>
+            </div>
+        </div>
+    </footer>
