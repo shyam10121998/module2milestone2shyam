469ff59c9158a6c71afefc86fe49be1950fae1b5
shyam10121998 <shyamrachchh77@gmail.com>
2023-10-17 15:36:43 -0800

    Add Navigation HTML Structure

---
+++ header.php
@@ -0,0 +1,14 @@
+<header>
+        <div class="logo">
+         <a href="index.html"> <img src="/images/logo.png"  alt="Logo"></a>  
+        </div>
+        <nav>
+            <ul class="menu">
+                <li><a href="products.html">Products</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="tips.html">Tips</a></li>
+                <li><a href="blog.html">Blog</a></li>
+                <li><a href="contact.html">Contact Us</a></li>
+            </ul>
+        </nav>
+    </header>
