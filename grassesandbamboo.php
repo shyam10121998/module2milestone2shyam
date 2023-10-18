59f1b57ecf5975e9aee9551a52a54c8dd7cf3426
shyam10121998 <shyamrachchh77@gmail.com>
2023-10-17 18:27:19 -0800

    Add Content For grassesandbamboo.view.php And Connect It To grassesandbamboo.php

---
+++ grassesandbamboo.php
@@ -0,0 +1,3 @@
+<?php require('header.php') ?>
+<?php require('views/grassesandbamboo.view.php') ?>
+<?php require('footer.php') ?>
---
+++ views/grassesandbamboo.view.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>                                    
+                                  <!--subcategory block-->       
+                                  
+       <div class="global content">
+        <h1 class="globalh1">Grasses an Bamboo-assorted</h1>
+        <div class="globalcards">
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Carex</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Fescue</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Hakonechloa</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Blood Grass</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Miscanthus</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Miscanthus</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Mondo</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Pampas</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Rufa </h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Green Panda</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Woolong</h2>
+                </div>
+            </div>
+
+           
+            
+        </div>
+    </div>        
+    
+</body>
+</html>
