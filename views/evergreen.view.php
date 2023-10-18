2429db364d02b7972e996b7a775e455a7ab2f9e4
shyam10121998 <shyamrachchh77@gmail.com>
2023-10-17 18:48:24 -0800

    Add HTML Structure For evergreen.view.php

---
+++ views/evergreen.view.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>                                  
+                                  <!--subcategory block-->       
+                                  
+       <div class="global content">
+        <h1 class="globalh1">Evergreen</h1>
+        <div class="globalcards">
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Marina and Unedo</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Bracken's Brown Beauty</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>DD Blanchard</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Edith Bogue</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Kay Paris</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Victoria</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Bright Lights</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Bonfire</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Gulfstream</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Firepower</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Delavyii</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Goshiki</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Burkwoodii</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Davidii</h2>
+                </div>
+            </div>
+            
+           
+        </div>
+    </div>        
+                
+</body>
+</html>
