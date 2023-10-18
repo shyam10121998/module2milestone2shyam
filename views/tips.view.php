b611163d7d4512ae6b963baca1beb6070d060cba
shyam10121998 <shyamrachchh77@gmail.com>
2023-10-17 17:29:01 -0800

    Add HTML Structure To tips.view.php

---
+++ views/tips.view.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>                                   
+                                            <!--tips-->       
+
+    <img class="banner" src="/images/banner.png" width="100%">
+
+    <section class="planting-tips content">
+        <h1>Planting Tips for Trees and Shrubs</h1>
+        <div class="tips" id="before-planting">
+            <h2>Before Planting:</h2>
+            <ul>
+                <li>Make sure your Trees & Shrubs are well watered before planting.</li>
+                <li>Place your new purchases in afternoon shade if you're unable to plant them right away during summer.</li>
+                <li>Never plant during the heat of the day, wait until evening.</li>
+                <li>Make sure you have the right location for your plant to thrive, check soil and exposure requirements.</li>
+                <li>Most plants don't do well in poor draining soil.</li>
+                <li>If you're unsure dig a test hole, fill it with water and see how quickly it drains.</li>
+            </ul>
+        </div>
+
+        <div class="tips" id="planting">
+            <h2>Planting:</h2>
+            <ul>
+                <li>Dig a hole that's as deep as the container or root-ball but twice as wide, if possible.</li>
+                <li>Place your new purchases in afternoon shade if you're unable to plant them right away during summer.</li>
+                <li>Remove the plant carefully from the container. Never break up the root system on trees and shrubs.</li>
+                <li>B&B trees should be planted intact, remove the string and roll back the burlap away from the trunk.  DO NOT REMOVE THE BURLAP</li>
+                <li>Bury the burlap it is biodegradable</li>
+                <li>Back fill the hole with the existing soil, for poor soils amend with a good quality bark mulch or compost.</li>
+                <li>Sandy soils can be amended with peat moss, for better water retention.</li>
+                <li>When back filling, make sure there are no air pockets or roots showing above the surface.</li>
+                <li>Do not plant too deep; covering the trunk too high can lead to serious problems.</li>
+                <li>Firm the soil gently around the plant, your foot works best.</li>
+                <li>Stake large trees to keep them stable and growing straight</li>
+                <li>Protect young trees from Deer and Rabbits.</li>
+                <li>Give your newly planted tree or shrub a good soaking, depending on the weather.</li>
+            </ul>
+        </div>
+
+        <div class="tips" id="after-care">
+            <h2>After Care:</h2>
+            <ul>
+                <li>Water regularly for at least the first two years after planting regardless if the plants are drought tolerant.</li>
+                <li>It's important to develop a good root system; your plants will be able to tolerate long dry spells better.</li>
+                <li>Water deeply and less often than a light sprinkle once a day, how often will depend on the weather.</li>
+                <li>Best time to water is in the evening, this will help keep the soil hydrated through the night.</li> 
+                <li>Fertilize with a slow release fertilizer in spring at least for the first five years.</li>
+            </ul>
+        </div>
+
+        <div class="tips" id="plant-spacing">
+            <h2>Plant spacing:</h2>
+            <ul>
+                <li>Boxwoods - 18"</li>
+                <li>Emeralds - 24"-30"</li>
+                <li>When planting a hedge spacing should not be any closer than 3 ft.  The taller you plan to grow your hedge, the wider the spacing.</li>
+                <li>Keep the on centre rule in mind - ie. if mature width is 15' then you can plant up to 8 feet apart.</li> 
+                <li>planting spacing also depends on how quickly you want full coverage and how much maintenance you wish to do.</li>
+            </ul>
+        </div>
+    </section>
+
+    
+</body>
+</html>
