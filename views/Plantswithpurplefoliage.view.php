73ae242fbb06dad7ef15efe0de31d8cdb5547712
shyam10121998 <shyamrachchh77@gmail.com>
2023-10-17 19:25:05 -0800

    Add HTML Structure to The File

---
+++ views/Plantswithpurplefoliage.view.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+                                         
+                                            <!--full-blog-->       
+
+    <div class="full-blog content">
+        <h1>Top 10 Plants with purple foliage</h1>
+        <P>Plants with purple leaves stand out, draw the eye and add a splash of color to the landscape. But if over used they become distracting and over powering, this also goes for blue and yellow foliage too. A garden should be mostly some shade of green with the brighter colored plants used as focal points. Depending on the size of the garden, you can use purple in more than one bed to create a balanced landscape that's pleasing to the eye. For example you might use a tree with purple foliage in one area and carry the color through to another bed with purple shrubs. When using purple make sure it doesn't fade into the background of a building. Example: purple foliage would not stand out against a red or dark brown house, while yellow, blue or green would. There are no conifers with year round purple foliage that I can think of, but there are some that turn purple in winter, like Cryptomeria Elagans which has green foliage in summer and a deep purple tint in winter. Note: Most trees and shrubs with purple foliage need afternoon sun for the best color.
+            <br><strong>1# Berberis 'Concorde' Berberis thunbergii: </strong>Rounded dwarf shrub with dark purple foliage and small yellow flowers in spring. A nice deer resistant shrub for rockeries or a border edge. All Berberis are drought tolerant and need full sun, well-drained soil.
+
+            <br><strong>2# Beech 'Purple Fountain'  Fagus sylvatica pendula:</strong>A graceful weeping specimen tree with deep purple foliage and attractive silver bark. Mature height 25' to 30' tall x 15' wide.  
+            
+            <br><strong>3# Elderberry 'Black Lace'  Sambucus nigra:</strong>  Medium sized shrub with lacy black-purple leaves and clusters of light-pink flowers in spring. A fast growing shrub with attractive delicate foliage resembling a Japanese maple. 
+
+
+            
+            <br><strong>4# Maple 'Bloodgood' Acer palmatum: </strong>A popular selection of Japanese maple with reddish-purple foliage and bright red fall color. Suitable for large containers or small gardens. Needs well-drained soil in winter.
+            
+            <br><strong>5# Maple 'Crimson Sentry' Acer platanoides:   </strong>Medium sized tree with a narrow canopy and dense deep purple foliage. A small enough tree for an average garden, rarely reaching over 25' tall x 15' wide. 
+            
+            <br><strong>6# Ninebark ' Diablo' Physocarpus opulifolius:</strong>Medium sized vase-shaped shrub with dark purple foliage. Small pinkish-white flowers in early summer. A nice specimen shrub which can be used as a hedge or screen.
+            
+            <br><strong>7# Ornamental Plum 'Pissardii' Prunus cerasifera: </strong>Medium sized tree with rounded canopy. Dark reddish-purple foliage with masses of pink flowers in spring. Best in full sun with very well-drained soil in winter.
+            
+            <br><strong>8# Redbud 'Ruby Falls' Cercis canadensis:  </strong>Beautiful small weeping tree with heart-shaped purple leaves, deep lavender flowers in spring. Patience is needed as Cercis are one of the last trees to leave out in spring. Needs full sun and very well drained soil in winter.  
+            
+            <br><strong>9# Smoke Bush 'Royal Purple'  Cotinus coggygria:</strong> Very large shrub with dark purple foliage turning scarlet in fall. Drought tolerant when established, can be pruned to maintain smaller size. 
+            
+            <br><strong>10# Weigela 'Midnight Wine' Weigela florida:</strong> Small shrub with dark purple foliage, attractive funnel-shaped pink flowers in late spring. Suitable for border edge or rockery.      </p>
+    </div>
+    
+</body>
+</html>
