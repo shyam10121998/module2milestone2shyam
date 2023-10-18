2c745456fc421737b7e39968b4d6fcf259837971
shyam10121998 <shyamrachchh77@gmail.com>
2023-10-17 19:22:29 -0800

    Add HTML Structure To Floweringdeciduoud.view.php And Connect To Floweringdeciduous.php

---
+++ Floweringdeciduoustrees.php
@@ -0,0 +1,3 @@
+<?php require('header.php') ?>
+<?php require('views/Floweringdeciduous.view.php') ?>
+<?php require('footer.php') ?>
---
+++ views/Floweringdeciduoustrees.view.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>                                   
+                                            <!--full-blog-->       
+
+    <div class="full-blog content">
+        <h1>Top 10 Flowering deciduous trees</h1>
+        <P>You know spring is here and can put the winter blues behind, at the sight of a deciduous tree in full bloom. While most deciduous trees flower in spring, there are some very nice summer flowering types. When choosing a tree make sure the mature size and spread will fit in the landscape. Ask if the tree is suitable for our climate, the Comox Valley is considered ZONE 7, there are micro climates that are warmer than the average Temperature and able to grow the more exotic types like the Empress tree. Important are soil conditions, most require well-drained soils in winter and do not tolerate standing water. Try to pick trees that offer more than just blooms, example interesting form or leaves, fall color or unique bark for seasonal interest. Don't over look planting fruit trees they put on a pretty good display, while the blooms may be short lived, you can always look forward to eating the fruit.
+            <br><strong>1# Fir 'Dwarf Balsam' Abies Balsamea nana:</strong>Rounded shrub with glossy dark bluish-green foliage, bright green new growth in spring. A nice shrub for shade gardens.
+
+            <br><strong>2# Cedrus 'Feeling Blue' Cedrus deodara:</strong> Low spreading shrub with blue-grey foliage. Suitable for any garden or trailing over rocks & walls. Prune to maintain spread.
+            
+            <br><strong>3# Crytomeria 'Tenzan' Cryptomeria japonica:</strong>Ball form with deep green foliage turning deep purple in winter. Small specimen shrub with winter interest.
+            
+            <br><strong>4# Hemlock 'Cole's Prostrate' tsuga canadensis:</strong> Low spreading form with trailing branches and dark green foliage. Good as a ground cover, container, bonsai or trailing over rocks & walls. Needs afternoon shade.
+            
+            <br><strong>5# Larch 'Little Boogle' Larix kaempferi:</strong> A deciduous conifer with contorted branches, attractive green foliage and golden-orange fall color. A unique shrub that's not truly a dwarf, reaching 2m tall in ten years, but deserves a spot in the garden. Perfect for containers or bonsai.
+            
+            <br><strong>6# Juniper 'Blue Star' Juniperus Squamata:</strong>Star shaped form with bright silver-blue foliage. Good as a border edge shrub, mass plantings or rockery.
+            
+            <br><strong>7# Pine 'Adcock's Dwarf' Pinus parviflora:</strong> Compact conical form with tiny greyish-green needles. A slow growing shrub for rockeries, containers or bonsai.
+            
+            <br><strong>8# Pine 'Sea Urchin' Pinus strobus:</strong> Rounded shrub with dense soft green foliage. Suitable as a small specimen, rockery or containers.
+            
+            <br><strong>9# Podpcarpus 'Chocolate Box' Podocarpus x:</strong>Rounded form with green foliage turning a attractive dark brown in winter. Small shrub with winter interest, suitable for rockery or containers.
+            
+            <br><strong>10# Spruce 'Little Gem' Picea abies:</strong> Bun-shaped small shrub with tiny dark green needles and bright green new growth. Tolerates both sun or shade.</p>
+    </div>
+       
+
+</body>
+</html>
