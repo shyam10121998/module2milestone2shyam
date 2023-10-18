b8c3b8e540664231e45dc8b93e11bd35aab652ca
shyam10121998 <shyamrachchh77@gmail.com>
2023-10-17 18:22:45 -0800

    Add HTML Structure For deciduos.view.php And Connect It To deciduous.php

---
+++ views/deciduous.view.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>                                      
+                                  <!--subcategory block-->       
+                                  
+       <div class="global content">
+        <h1 class="globalh1">Deciduous</h1>
+        <div class="globalcards">
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2> Silk Tree</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Dawyck Gold</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Dawyck Green</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Dawyck Purple</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Weeping Purple Fountain</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Bailone</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Concorde</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Rose Glow</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Sunsation</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Royal Cloak</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Gentry</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>weeping sweetshrub</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Bean Tree</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Forest Pansy</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Kwanzan</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Mount Fuji</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Royal Purple</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Young Lady</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Velveteeny</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Royal Cloak</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Old-Fashioned</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Grace</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Golden Rain</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Whitney</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Arctic Fire</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Winter Sun</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Burning Bush</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Troll</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Weeping Wonder</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Roswitha</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2> Flying Dragon</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Pissardii</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Twisty Baby</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Frisia</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Black Tower</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Black Lace</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Japanese Pseudocamellia</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Japanese Snowbell</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Pink Weeping</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2> Little Leaf Linden</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Spring Bouquet</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Dawn</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2> Arnolds Promise</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Diane</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Jelena</h2>
+                </div>
+            </div>
+            
+        </div>
+    </div>        
+    
+</body>
+</html>
