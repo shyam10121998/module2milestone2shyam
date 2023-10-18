eb429d4a3d5ed1f8ced8ccdabd3f63adb1ee8149
shyam10121998 <shyamrachchh77@gmail.com>
2023-10-17 19:02:43 -0800

    Add HTML And CSS For WeepingJapaneseMaples.php

---
+++ views/WeepingJapaneseMaples,view.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>                                      
+                                            <!--full-blog-->       
+
+    <div class="full-blog content">
+        <h1>Top 10 Weeping Japanese Maples</h1>
+        <P>There are so many available on the market today it's a little overwhelming. They come in large variety of shapes and colors. The weeping or cascading forms are usually low growing with a mushroom-shaped form. Most but not all have delicate laceleaf foliage and are suitable for mixed borders, containers, rockeries or cascading over a water feature. Easy to care for, all they need is well-drained soil (never water-logged conditions) and sun exposure depends on the cultivar. Young plants may scorch in hot afternoon sun, especially on a deck or concrete patio. Resembling drought, the fine leaves curl and turn brown at the tips, if severe enough the whole leave is affected. Don't panic and over water, they will become resistant to this as they get older. Give them room to mature, while considered a small maple they can develop a large spread with age, or simply prune and shape to maintain a smaller size.
+            <br><strong>1#</strong> 'Crimson Queen' Acer palmatun dissectum: Laceleaf cascading type with dense wine-red foliage turning brilliant red in fall. Very popular selection and easy to grow.
+
+            <br><strong>2#</strong>  'Green Cascade' Acer japonicum: A unique weeping maple with broad dissected leaves. The green foliage turns bright yellow, orange and red in fall. For more height stake the center leader. Related to the full moon cultivars its best planted in light afternoon shade to avoid scorched foliage.
+            
+            <br><strong>3#</strong> 'Inaba Shidare' Acer palmatum dissectum: An old time favorite that's been around for awhile. A vigorous and fairly fast growing weeping laceleaf maple, with wine-red foliage turning red in fall.
+            
+            <br><strong>4#</strong> 'Lionheart' Acer palmatum dissectum: An unusual selection of laceleaf maple with reddish-purple foliage and red fall color. The weeping branches are similar to other dissectums, what makes it different is the height, growing taller with a layered look.
+            
+            <br><strong>5#</strong> 'Orangelo' Acer palmatum dissectum: The chameleon of the maple world this cascade maple changes color all season. The laceleaf foliage is orange-red in spring, turning purplish-green in summer, then red and finishing bright orange-red in fall. A vigorous grower when young, but slows down with age, also one of the last to lose its leaves in fall.
+            
+            <br><strong>6#</strong> 'Red Dragon' Acer palmatum dissectum: Intense reddish-purple foliage with scarlet spring and fall color. An attractive slow growing laceleaf weeper, it remains purple even in shade. Similar to crimson queen in form but smaller.
+            
+            <br><strong>7#</strong> 'Ryusen' Acer palmatum: Unique, unusual describe this dwarf weeping maple. Unlike the laceleaf types this little gem has small bright green palmatum leaves, turning a brilliant golden-orange and red in fall. Can be trained to cascade over rocks and walls or staked for height.
+            
+            <br><strong>8#</strong> 'Tamuke yama' Acer palmatum dissectum: Similar to Inaba shidare in form, this dark purple-red laceleaf weeper has grayish bark. Like Inaba Shidare it can get tall and wide with age, so give it room to mature.
+            
+            <br><strong>9#</strong> 'Toyama nishiki' Acer palmatum dissectum: Different is the word to describe this laceleaf weeping maple. What makes it unique is the variegated foliage. The basic purple-green leaves are splashed with pink and white in spring, turning orange-red in fall. It needs a sheltered area in light afternoon shade. Not as hardy and needing more care than others types its uniqueness makes it worth the effort.
+            
+            <br><strong>10#</strong> 'Waterfall' Acer palmatum dissectum: A green version of Crimson queen, this hardy bright green laceleaf weeper turns an attractive golden-orange in fall.</p>
+    </div>
+      
+
+</body>
+</html>
