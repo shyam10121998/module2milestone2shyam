335bbfaa33a87698ffbcaa36ae6299c2b9e71bb2
shyam10121998 <shyamrachchh77@gmail.com>
2023-10-17 18:29:48 -0800

    Add HTML Structure For vines.view.php

---
+++ views/vines.view.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+                                           
+                                  <!--subcategory block-->       
+                                  
+       <div class="global content">
+        <h1 class="globalh1">Vines-assorted</h1>
+        <div class="globalcards">
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Clematis</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Honeysuckle</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Rose</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Wisteria</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Hydrangea</h2>
+                </div>
+            </div>
+
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 2">
+                <div class="globalcard-text">
+                    <h2>Hops</h2>
+                </div>
+            </div>
+            <div class="globalcard">
+                <img src="images/product category.png" alt="Image 1">
+                <div class="globalcard-text">
+                    <h2>Jasmine</h2>
+                </div>
+            </div>
+
+           
+            
+        </div>
+    </div>        
+      
+</body>
+</html>
