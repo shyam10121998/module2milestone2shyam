3facbac3b6e0948230e3058f9d1e52e8693e792a
shyam10121998 <shyamrachchh77@gmail.com>
2023-10-31 21:42:23 -0800

    Update Social-icons With Font-awesome Icons

--- footer.php
+++ footer.php
@@ -3,9 +3,9 @@
             <h1>Outback Nursery</h1>
             <p>Nature's Palette, Our Expertise</p>
             <div class="social-icons">
-                <a href="tel:2507030723" target="_blank"><img src="images/phone.png" alt="Phone"></a>
-                <a href="https://www.facebook.com/p/Outback-Nursery-Garden-Centre-100045470087619/" target="_blank"><img src="images/facebook-icon-512x512-seb542ju.png" alt="Facebook"></a>
-                <a href="https://www.instagram.com/outbacknursery/?hl=en" target="_blank"><img src="images/Instagram-Icon.png" alt="Instagram"></a>
+                <a href="tel:2507030723" target="_blank"><i class="fas fa-phone"></i></a>
+                <a href="https://www.facebook.com/p/Outback-Nursery-Garden-Centre-100045470087619/" target="_blank"><i class="fab fa-facebook"></i></a>
+                <a href="https://www.instagram.com/outbacknursery/?hl=en" target="_blank"><i class="fab fa-instagram"></i></a>
             </div>
         </div>
     </footer>
