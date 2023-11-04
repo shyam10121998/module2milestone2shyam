254f7caac5cce586b97598ea4666ffef9c08fa2c
shyam10121998 <shyamrachchh77@gmail.com>
2023-11-03 21:37:33 -0800

    Add Code For Data Of Each Row

--- login_submit.php
+++ login_submit.php
@@ -7,3 +7,20 @@ if ($_POST['password'] && $_POST['username']) {
 
     $checkUserExist = "SELECT `id` from `users` WHERE `username` = '{$_POST['username']}' AND `password` = '{$hashPassword}' LIMIT 1";
     $result = $mysqli->query($checkUserExist);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            session_start();
+            $_SESSION['user_id'] = $row['id'];
+        }
+    } else {
+        echo "USER NOT FOUND";
+        die;
+    }
+}
+
+
+
+
+header("Location:index.php");
