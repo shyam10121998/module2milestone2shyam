f974c5496bc38214eeeba5f604a43fec9d289226
shyam10121998 <shyamrachchh77@gmail.com>
2023-11-04 16:39:34 -0800

    Create logout.php File

---
+++ login-header.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="logo">
+        <a href="index.php"> <img src="images/logo.png" alt="Logo"></a>
+    </div>
+    <nav>
+        <ul class="menu">
+            <li><a href="registration.php" style="padding-right: 25px;">Registration</a></li>
+
+        </ul>
+    </nav>
+</header>
