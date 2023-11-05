bb7c62b6d8fceb2ef82150df5fa4ef1eb43147ef
shyam10121998 <shyamrachchh77@gmail.com>
2023-11-05 14:21:40 -0800

    Update Content For Services. Create 2*2 Grid

--- views/services.view.php
+++ views/services.view.php
@@ -3,70 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@1,800&family=Oswald&family=Rubik&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>                                
                                          <!--Services block-->  
-    <div class="services content">  
-            <h1 class="servicesh1">Services We Offer</h1> 
+                                         <div class="services content">  
+        <h1 class="servicesh1">Services We Offer</h1> 
 
-        <div class="services-card content" id="service1">
-            <div class="services-title">New services</div>
-            <div class="services-details">In addition to our expertise in providing a wide variety of plants, we have expanded our offerings to include a delightful range of garden accessories. Our collection now features decorative pots, inviting stepping stones, charming birdbaths, comfortable benches, and an enchanting array of garden ornaments and statuary. These additions are carefully curated to enhance the beauty and ambiance of your outdoor spaces. Explore our garden accessory selection to find the perfect elements that will transform your garden into a picturesque haven, adding an extra touch of elegance and personality to your landscape.</div>
-        </div>
+        <div class="services-row">
+            <div class="services-card" id="service1">
+                <div class="services-title">New services</div>
+                <div class="services-details">In addition to our expertise in providing a wide variety of plants, we have expanded our offerings to include a delightful range of garden accessories. Our collection now features decorative pots, inviting stepping stones, charming birdbaths, comfortable benches, and an enchanting array of garden ornaments and statuary. These additions are carefully curated to enhance the beauty and ambiance of your outdoor spaces. Explore our garden accessory selection to find the perfect elements that will transform your garden into a picturesque haven, adding an extra touch of elegance and personality to your landscape.</div>
+            </div>
 
-        <div class="services-card" id="service2">
-            <div class="services-title">Prompt services</div>
-            <div class="services-details">For prompt service and the best selection of the season, we recommend that you attend the garden centre as often as your can.   We're a fun place to spend time and the sights are ever changing!. 
-                Regular visits are recommended.
-               
-               If you have a question about a specific tree you'd like to plant, the best first step is learning as much as you can from online sources before your visit.
-                
-               Some of our favourite websites!
-                
-               rhs.org.uk
-               gardenia.net
-               finegardening.com
-               pinterest.com
-               houzz.com</div>
-        </div>
+            <div class="services-card" id="service2">
+                <div class="services-title">Prompt services</div>
+                <div class="services-details">For prompt service and the best selection of the season, we recommend that you attend the garden centre as often as you can. We're a fun place to spend time and the sights are ever changing!. Regular visits are recommended. If you have a question about a specific tree you'd like to plant, the best first step is learning as much as you can from online sources before your visit. Some of our favorite websites: rhs.org.uk, gardenia.net, finegardening.com, pinterest.com, houzz.com</div>
+            </div>
 
+            <div class="services-card" id="service3">
+                <div class= "services-title">GIFT CERTIFICATES</div>
+                <div class="services-details">Exciting news! Outback Nursery now offers gift certificates, making it easier than ever to share the joy of gardening and beautiful plants with your loved ones. Here's how to get one in just three simple steps:
+                    <br>1] Send an email to info@outbacknursery.ca with your details, including the recipient's and sender's names, preferred timeline, desired amount, and a short, special message if you'd like to add a personal touch.
+                    <br>2] Make a secure e-transfer to info@outbacknursery.ca for the chosen gift certificate amount.
+                    <br>3] Once we receive your e-transfer, we'll promptly send you a scanned voucher via email, or we can send it to another email address of your choice. Whether you want to leave the "TO" and "FROM" fields blank for personalization or have us fill them in based on your provided details, we're here to accommodate your preferences.</div>
+            </div>
 
-        <div class="services-card" id="service3">
-            <div class="services-title">GIFT CERTIFICATES</div>
-            <div class="services-details">Exciting news! Outback Nursery now offers gift certificates, making it easier than ever to share the joy of gardening and beautiful plants with your loved ones. Here's how to get one in just three simple steps:
+            <div class="services-card" id="service5">
+                <div class="services-title">Bark Mulch Sales and Delivery</div>
+                <div class="services-details">Discover our premium FIR/HEMLOCK bark mulch blend, the perfect choice for enhancing your garden's beauty and health. Priced at just $7.00 per bag or $50.00 per yard when you bring your truck or trailer, it's a cost-effective solution for all your mulching needs. For your convenience, we recommend calling ahead at 250-218-8015 to ensure our equipment operator is on-site to assist you. Elevate your landscaping with this quality bark mulch blend and enjoy a flourishing garden.</div>
+            </div> 
 
-               <br><strong>1]Send an email to info@outbacknursery.ca with your details, including the recipient's and sender's names, preferred timeline, desired amount, and a short, special message if you'd like to add a personal touch.
-                
-                <br>2]Make a secure e-transfer to info@outbacknursery.ca for the chosen gift certificate amount.
-                
-                <br>3]Once we receive your e-transfer, we'll promptly send you a scanned voucher via email, or we can send it to another email address of your choice.</strong>
-                
-                Whether you want to leave the "TO" and "FROM" fields blank for personalization or have us fill them in based on your provided details, we're here to accommodate your preferences. Thank you for choosing Outback Nursery for your gifting needs!</div>
-        </div>
-
-        <div class="services-card" id="service4">
-            <div class="services-title">Clearance</div>
-            <div class="services-details">
-                <h3>ALL CONCRETE is 35% off</h3>
-                <p>Come in to see our selection of benches, statuary, wall art, bird baths and stepping stones!</p>
+            <div class="services-card" id="service6">
+                <div class="services-title">Return policy</div>
+                <div class="services-details">Our policy regarding live plant materials is that we do not offer refunds or accept returns. Rest assured, we take great care to ensure that our plants are in excellent health when they leave our nursery. We kindly request that you continue to maintain their well-being through proper care and attention. Your commitment to nurturing these plants will help them thrive and flourish in their new environment. Should you have any questions or concerns about their care, our team is always here to provide guidance and support to ensure your plants continue to thrive.</div>
             </div>
         </div>
-
-        
-        <div class="services-card" id="service5">
-            <div class="services-title">Bark Mulch Sales and Delivery</div>
-            <div class="services-details">Discover our premium FIR/HEMLOCK bark mulch blend, the perfect choice for enhancing your garden's beauty and health. Priced at just $7.00 per bag or $50.00 per yard when you bring your truck or trailer, it's a cost-effective solution for all your mulching needs. For your convenience, we recommend calling ahead at 250-218-8015 to ensure our equipment operator is on-site to assist you. Elevate your landscaping with this quality bark mulch blend and enjoy a flourishing garden.</div>
-        </div> 
-
-        <div class="services-card" id="service6">
-            <div class="services-title">Return policy</div>
-            <div class="services-details">Our policy regarding live plant materials is that we do not offer refunds or accept returns. Rest assured, we take great care to ensure that our plants are in excellent health when they leave our nursery. We kindly request that you continue to maintain their well-being through proper care and attention. Your commitment to nurturing these plants will help them thrive and flourish in their new environment. Should you have any questions or concerns about their care, our team is always here to provide guidance and support to ensure your plants continue to thrive.</div>
-        </div>     
-        
-    </div>         
-        
-
+    </div>
 </body>
 </html>
