28cd993fc5502d6b19889280a37de65ce9014eeb
shyam10121998 <shyamrachchh77@gmail.com>
2023-11-04 17:02:24 -0800

    Introduce if Condition

--- question_submit.php
+++ question_submit.php
@@ -5,3 +5,18 @@ require('./db_connection.php');
 $question = $_POST['question'];
 
 session_start();
+if (isset($_SESSION['user_id'])) {
+
+    if ($question) {
+        $created_at = time();
+
+        $insertQuestionSql = "INSERT INTO `questions` (`question`,`created_at`,`user_id`) VALUE ('{$question}' , $created_at , {$_SESSION['user_id']})";
+
+        $mysqli->query($insertQuestionSql);
+
+        header("Location:index.php");
+    }
+} else {
+
+    header("Location:login.php");
+}
