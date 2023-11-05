913853d2ab4afe0e8d843509298e919a7ed0eff1
shyam10121998 <shyamrachchh77@gmail.com>
2023-11-04 19:59:56 -0800

    Add Query To Fetch The Data

--- views/blog.view.php
+++ views/blog.view.php
@@ -18,5 +18,42 @@ $results = $mysqli->query($blogSql);
 
 ?>
 
+<img class="banner" src="images/banner.png" width="100%">
+
+<h1 class="blogh1 content">The following lists were written and published (Comox Valley Record) by Annette (2018).
+    We hope they provide a great reference when planning your outdoor spaces.</h1>
+
+
+    <?php
+
+while ($row = $results->fetch_assoc()) {
+    if ($row['id'] % 2 != 0) {
+        echo "  <div class='section content'>
+            <div class='image'>
+                <img src='images/{$row['image']}' alt='Image 1'>
+            </div>
+            <div class='text'>
+                <h3 class='blogh3'>{$row['title']}</h3>
+                <p>{$row['description']}</p>
+                <a href='{$row['link']}' class='button'>Read Full</a>
+            </div>
+        </div>";
+    } else {
+
+        echo "  <div class='section content'>
+            <div class='text'>
+                <h3 class='blogh3'>{$row['title']}</h3>
+                <p>{$row['description']}</p>
+                <a href='{$row['link']}' class='button'>Read Full</a>
+            </div>
+            <div class='image'>
+                <img src='images/{$row['image']}' alt='Image 2'>
+            </div>
+        </div> ";
+    }
+}
+
+?>
+
 </body>
 </html>
