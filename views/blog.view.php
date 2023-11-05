dd2934b2530270f1d2fc139c7d3bb3575947f652
shyam10121998 <shyamrachchh77@gmail.com>
2023-11-05 15:38:53 -0800

    Update Changes To Blog Page

--- views/blog.view.php
+++ views/blog.view.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@1,800&family=Oswald&family=Rubik&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
@@ -18,7 +21,7 @@ $results = $mysqli->query($blogSql);
 
 ?>
 
-<img class="banner" src="images/banner.png" width="100%">
+<img class="banner" src="images/annetteblogbanner.jpg" width="100%" height="500">
 
 <h1 class="blogh1 content">The following lists were written and published (Comox Valley Record) by Annette (2018).
     We hope they provide a great reference when planning your outdoor spaces.</h1>
