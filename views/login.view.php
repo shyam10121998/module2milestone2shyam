0973485a9e39087b19bec78cb3a42f9c27b920f6
shyam10121998 <shyamrachchh77@gmail.com>
2023-11-05 15:08:50 -0800

    Update login.view.php

--- views/login.view.php
+++ views/login.view.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@1,800&family=Oswald&family=Rubik&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 
     <title>Document</title>
