d6a0e1050a26cbf15396a4bdf739339d0e14cbbe
shyam10121998 <shyamrachchh77@gmail.com>
2023-11-04 22:17:09 -0800

    Gave internal CSS Styling Temporarily

--- views/registration.view.php
+++ views/registration.view.php
@@ -7,6 +7,64 @@
     <link rel="stylesheet" href="style.css">
 
     <title>Document</title>
+    <style>
+        header {
+            position: absolute;
+            width: 100%;
+        }
+
+        footer {
+            position: absolute;
+            bottom: 0px;
+            width: 100%;
+        }
+
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .registration-container {
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+            padding: 20px;
+            height: 500px;
+            text-align: center;
+            position: absolute;
+            top: 106px;
+        }
+
+        input {
+            width: 100%;
+            padding: 10px;
+            margin: 10px 0;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+        }
+
+        button.button {
+            display: block;
+            width: 90%;
+            align-items: center;
+            padding: 10px;
+            margin: 5px;
+            background-color: gray;
+            border: none;
+            color: #fff;
+            border-radius: 3px;
+            text-decoration: none;
+            cursor: pointer;
+            position: relative;
+            top: 38px;
+        }
+    </style>
 </head>
 
 
